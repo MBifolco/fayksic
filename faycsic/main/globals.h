50a545b27baf2954ca8c72fa179a6cd04f7df5ed
Juan Gutierrez <juanny.gee@gmail.com>
2025-01-05 12:58:09 -0500

    Minor reorganization
    * Clear things out of `main.c`
    * Reaname hash to `mine_block_task`
    * Move `receive_task` into its own file
    * Rename `mock_hash.h` to `mock_messages.h`

--- faycsic/main/globals.h
+++
@@ -1,9 +0,0 @@
-#ifndef QUEUE_HANDLES_H
-#define QUEUE_HANDLES_H
-
-#include "freertos/FreeRTOS.h"
-#include "freertos/queue.h"
-
-extern QueueHandle_t work_queue;
-extern uint32_t target_difficulty[2];
-#endif
